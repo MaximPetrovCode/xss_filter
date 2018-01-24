d9390ee511581757eab9ae74c3e6f41617ea5761
max <max@xxx.com>
2018-01-24 14:03:21 +0300

    be carefull with xss

---
+++ test.php
@@ -0,0 +1,62 @@
+
+<?php
+
+
+function clean($data)
+{
+    // Delete namesapced elements
+    $data = preg_replace('#</*\w+:\w[^>]*+>#i', '', $data);
+
+    // Clear from javascript and vbscript
+    $data = preg_replace('#([a-z]*)[\x00-\x20]*=[\x00-\x20]*([`\'"]*)[\x00-\x20]*j[\x00-\x20]*a[\x00-\x20]*v[\x00-\x20]*a[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2nojavascript...', $data);
+    $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*v[\x00-\x20]*b[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2novbscript...', $data);
+    $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*-moz-binding[\x00-\x20]*:#u', '$1=$2nomozbinding...', $data);
+
+    // Fix &entity\n;
+    $data = str_replace(array('&amp;','&lt;','&gt;'), array('&amp;amp;','&amp;lt;','&amp;gt;'), $data);
+    $data = preg_replace('/(&#*\w+)[\x00-\x20]+;/u', '$1;', $data);
+    $data = preg_replace('/(&#x*[0-9A-F]+);*/iu', '$1;', $data);
+    $data = html_entity_decode($data, ENT_COMPAT, 'UTF-8');
+    
+    // Remove any attribute starting with "on" or xmlns
+    $data = preg_replace('#(<[^>]+?[\x00-\x20"\'])(?:on|xmlns)[^>]*+>#iu', '$1>', $data);
+    
+    // Getting rid of EI varantability
+    $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?expression[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+    $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?behaviour[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+    $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:*[^>]*+>#iu', '$1>', $data);
+
+
+    do
+    {
+        // Remove really unwanted tags
+        $old_data = $data;
+        $data = preg_replace('#</*(?:applet|b(?:ase|gsound|link)|embed|frame(?:set)?|i(?:frame|layer)|l(?:ayer|ink)|meta|object|s(?:cript|tyle)|title|xml)[^>]*+>#i', '', $data);
+    }
+    while ($old_data !== $data);
+
+    // we are done...
+    return $data;
+}
+
+function xss_filter($data) {
+    for($i=0;$i<count($data);$i++){
+        for ($j=0; $j < count($data[$i]); $j++) { 
+                $data[$i][$j] = clean($data[$i][$j]);
+        }        
+    }
+
+    return $data;
+}
+
+$data = [['value1','value2'],['<script>console.log("LoL")</script>', '<script>document.alert("You were not hacked ;)")</script>', '<script>Because there are no tags :)</script>', 'text4', 'text5'],['boom1','boom2']];
+$data = xss_filter($data);
+
+for($i=0;$i<count($data);$i++){
+    for ($j=0; $j < count($data[$i]); $j++) { 
+            echo $data[$i][$j];
+            echo "\n";
+        }        
+}
+
+?>
